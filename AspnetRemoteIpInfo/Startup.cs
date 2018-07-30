0b68f18b9b39305794aa78be0d256c1eb7a30120
rsantosdev <rsantos@braziliandevs.com>
2018-07-30 11:30:42 -0300

    Disabled all https stuff

--- AspnetRemoteIpInfo/Startup.cs
+++ AspnetRemoteIpInfo/Startup.cs
@@ -31,14 +31,14 @@ namespace AspnetRemoteIpInfo
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                app.UseHsts();
-            }
+            //if (env.IsDevelopment())
+            //{
+            //    app.UseDeveloperExceptionPage();
+            //}
+            //else
+            //{
+            //    app.UseHsts();
+            //}
 
             //app.UseHttpsRedirection();
             app.UseMvc();
