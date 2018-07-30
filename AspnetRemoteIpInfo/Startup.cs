f0365f54a9cfa41ee362b7fbf8b680b4f7e65e35
rsantosdev <rsantos@braziliandevs.com>
2018-07-30 11:20:27 -0300

    Disabling https force

--- AspnetRemoteIpInfo/Startup.cs
+++ AspnetRemoteIpInfo/Startup.cs
@@ -40,7 +40,7 @@ namespace AspnetRemoteIpInfo
                 app.UseHsts();
             }
 
-            app.UseHttpsRedirection();
+            //app.UseHttpsRedirection();
             app.UseMvc();
         }
     }
