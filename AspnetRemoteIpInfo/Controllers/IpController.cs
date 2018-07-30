5e8464fd5aa7780dc6d084b7eda82c3df3d03d21
rsantosdev <rsantos@braziliandevs.com>
2018-07-30 09:21:19 -0300

    Initial commit

---
+++ AspnetRemoteIpInfo/Controllers/IpController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspnetRemoteIpInfo.Controllers
+{
+    [Route("api/ip")]
+    public class IpController : ControllerBase
+    {
+        [HttpGet("")]
+        public IActionResult GetIp()
+        {
+            return Ok(new
+            {
+                ClientIp = HttpContext.Connection.RemoteIpAddress.ToString()
+            });
+        }
+    }
+}
